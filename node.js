3d58b9f28fb03d2661605ee8c8466a869531693b
mohanarajan1990 <99423268+mohanarajan1990@users.noreply.github.com>
2022-03-03 20:02:42 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,66 @@
+//MAP;
+// let a = [2, 4, 6, 8, 10];
+
+// a.map((x) => {
+//     console.log(x);
+// });
+
+
+
+// let a = [2, 4, 6, 8, 10];
+
+// let c = a.map((x) => {
+//     console.log(x);
+//     return x;
+// });
+// console.log(c);
+
+
+
+
+
+// let a = [2, 4, 6, 8, 10];
+
+// let c = a.map((x) => {
+//     console.log(x * 5);
+//     return x;
+// });
+// console.log(c);
+
+
+
+//filter
+// let a = [2, 4, 6, 8, 10];
+// let c = a.filter(function (x) {
+//     if (x > 4) return x;
+// });
+
+// console.log(c);
+
+
+//REDUCE;
+// let a = [2, 4, 6, 8, 10];
+// let c1 = a.reduce((a,c) =>a+c);
+// console.log(c1);
+
+
+// //CHANNING;
+// let a = [2, 4, 6, 8, 10];
+
+
+// let r = a.map((x) => x * 5);
+// let r1 = r.filter((x) => x * 20);
+// let r2 = r1.reduce((x) => x * 2);
+// console.log(r);
+// console.log(r1);
+// console.log(r2);
+
+//SET;
+let a = [1, 2, 3, 4, 5, 1, 3, 4, 3];
+
+let S = new Set(a);
+
+console.log(S);
+
+
+
